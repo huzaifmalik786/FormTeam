4248e9cd2b70672f2d783c68caa8dfe05700ea73
unknown <huzaifmalik7860@gmail.com>
2023-01-14 14:17:10 +0530

    searchbarimprovement

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,17 +10,23 @@ function SearchBar({setsearchdata}) {
     const handlesearch=()=>{
         const truedata=[];
         for(let i=0;i<1000;i++){
-            const v= value.replace(/ +/g,'');
-            const name= Data[i].first_name.trim() + Data[i].last_name.trim();
-            if(Data[i].first_name.toLowerCase()===v.toLowerCase()){
+            const a= value.replace(/ +/g,'');
+            const b= Data[i].first_name.trim() + Data[i].last_name.trim();
+            const name= b.toLowerCase();
+            const v= a.toLowerCase();
+            console.log(name.indexOf(v));
+            if(name.indexOf(v)===0){
                 truedata.push(Data[i]);
             }
-            else if(Data[i].last_name.toLowerCase()===v.toLowerCase()){
-                truedata.push(Data[i]);
-            }
-            else if(name.toLowerCase()===v.toLowerCase()){
-                truedata.push(Data[i]);
-            }
+            // if(Data[i].first_name.toLowerCase()===v.toLowerCase()){
+            //     truedata.push(Data[i]);
+            // }
+            // else if(Data[i].last_name.toLowerCase()===v.toLowerCase()){
+            //     truedata.push(Data[i]);
+            // }
+            // else if(name.toLowerCase()===v.toLowerCase()){
+            //     truedata.push(Data[i]);
+            // }
         }
         if(truedata.length){
             setsearchdata(truedata);
